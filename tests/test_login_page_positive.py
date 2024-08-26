7e23f242e9ab9ee60877bbc3edf754c567663ab7
Peter Chay <peterchay88@gmail.com>
2024-08-25 20:49:54 -0600

    SOS-5 Added pytest.ini file and set log level to info

--- tests/test_login_page_positive.py
+++ tests/test_login_page_positive.py
@@ -1,6 +1,6 @@
 import pytest
 from page_objects.login_page import LoginPage
-import logging
+import logging as logger
 
 pytestmark = [pytest.mark.login, pytest.mark.positive]
 
@@ -13,6 +13,7 @@ class TestLoginPagePositive:
         1. Enter Username, 2. Enter Password, 3. Click login, 4. Validate successful login
         :return:
         """
+        logger.info("Running test 1")
         login_page = LoginPage(driver=my_driver)
         login_page.open_login_page()
         login_page.enter_username(username="standard_user")
