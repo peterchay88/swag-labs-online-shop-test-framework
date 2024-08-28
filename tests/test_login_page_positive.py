ae223ba0077d922dec5fbd6a07873ca56c043de3
Peter Chay <peterchay88@gmail.com>
2024-08-28 14:12:21 -0600

    SOS-8 added parametrize but need to figure out why it does not call each test individually

--- tests/test_login_page_positive.py
+++ tests/test_login_page_positive.py
@@ -7,15 +7,19 @@ pytestmark = [pytest.mark.login, pytest.mark.positive]
 
 class TestLoginPagePositive:
 
-    def test_login(self, my_driver):
+    @pytest.mark.parametrize("username, password, test_id", [
+        pytest.param("standard_user", "secret_sauce", "1", marks=pytest.mark.tcid01),
+        pytest.param("visual_user", "secret_sauce", "2", marks=pytest.mark.tcid02)
+    ])
+    def test_login(self, my_driver, username, password, test_id):
         """
         This test confirms that we can log into the swag labs online shop
         1. Enter Username, 2. Enter Password, 3. Click login, 4. Validate successful login
         :return:
         """
-        logger.info("Running test 1")
+        logger.info(f"Running test {test_id}")
         login_page = LoginPage(driver=my_driver)
         login_page.open_login_page()
-        login_page.enter_username(username="standard_user")
-        login_page.enter_password(password="secret_sauce")
+        login_page.enter_username(username=username)
+        login_page.enter_password(password=password)
         login_page.click_login_button()  # TODO: Fix function
