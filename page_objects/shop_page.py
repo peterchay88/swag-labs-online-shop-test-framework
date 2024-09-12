6448bf3ad24dda9b11320412ae0fcc31bf1a4993
Peter Chay <peterchay88@gmail.com>
2024-09-12 10:01:13 -0600

    SOS-6 added nav bar element variable and method

--- page_objects/shop_page.py
+++ page_objects/shop_page.py
@@ -6,6 +6,7 @@ import logging as logger
 
 class ShopPage(BasePage):
     __cart_button = (By.ID, "shopping_cart_container")
+    __nav_bar = (By.ID, "react-burger-menu-btn")
 
     def __init__(self, driver: WebDriver):
         self.driver = driver
@@ -16,6 +17,7 @@ class ShopPage(BasePage):
         This method checks to see if the cart button is displayed on the shop page
         :return:
         """
+        logger.info("Checking to see if cart button element is present on web page")
         return super()._is_displayed(element=self.__cart_button)
 
     def click_cart_button(self):
@@ -23,5 +25,29 @@ class ShopPage(BasePage):
         This method is for clicking the cart button on the shop page
         :return:
         """
+        self.check_if_cart_button_is_displayed()
         logger.info("Clicking cart button")
         super()._click_button(element=self.__cart_button)
+
+    def is_logout_button_present(self):
+        pass
+
+    def click_logout_button(self):
+        pass
+
+    def is_nav_bar_present(self) -> bool:
+        """
+        This method checks to see if the nav bar on the shop page is present
+        :return:
+        """
+        logger.info("Checking to see if nav bar is present")
+        return super()._is_displayed(element=self.__nav_bar)
+
+    def click_nav_bar(self):
+        """
+        This method clicks on the nav bar on the shop page
+        :return:
+        """
+        self.is_nav_bar_present()
+        logger.info("Clicking nav bar")
+        super()._click_button(element=self.__nav_bar)
