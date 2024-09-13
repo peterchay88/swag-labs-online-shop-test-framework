2198e5a3af23937d69c4c666d834b9cd1e36641e
Peter Chay <peterchay88@gmail.com>
2024-09-12 23:07:24 -0600

    SOS-18 added test_shop_page.py

---
+++ tests/test_shop_page.py
@@ -0,0 +1,15 @@
+import pytest
+from page_objects.shop_page import ShopPage
+import logging as logger
+
+
+class TestShopPage:
+
+    def test_add_items_to_cart_from_main_page(self):
+        """
+        This test confirms that we are able to add items to the cart from the main shop page
+        1. Users can click add to cart for the items
+        2. Cart Icon updates with the number of items added
+        :return:
+        """
+        pass
