64a5bcfc0ccdb521ca390663ee18edf07d10d7e3
Peter Chay <peterchay88@gmail.com>
2024-09-26 15:28:23 -0600

    resolved chrome driver issue by introducing ChromeDriverManager

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,13 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(scope="session")
 def my_driver(request):
     if request.config.getoption("--browser") == "chrome":
-        my_driver = webdriver.Chrome()
+        my_driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     elif request.config.getoption("--browser") == "firefox":
         my_driver = webdriver.Firefox()
     else:
